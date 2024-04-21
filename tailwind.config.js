88f392d8f8cabe3e8525f54e9c32ee8fd9830856
Jan Krapac <jan.krapac@gmail.com>
2024-04-21 21:55:50 +0200

    Message, styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
